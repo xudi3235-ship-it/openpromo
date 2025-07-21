df73a84a133dc8184efbe4d5628e8a4698d431d5
Ruize Li <rl3250@columbia.edu>
2025-07-20 20:49:26 -0700

    sync

--- packages/www/src/routes/index.tsx
+++ packages/www/src/routes/index.tsx
@@ -1,12 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
-export const Route = createFileRoute('/')({
+import { createFileRoute } from "@tanstack/react-router";
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   return (
     <main>
-      <h1 className="text-3xl text-blue-500 mb-5">Hello world!</h1>
+      <h1 className="text-3xl text-blue-500 mb-5">Hello from Openpromo</h1>
     </main>
-  )
+  );
 }
