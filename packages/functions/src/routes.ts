e194b3002746be9dc6ff8f0f7ed7c4ffc3ccf871
Ruize Li <rl3250@columbia.edu>
2025-08-02 13:19:31 -0700

    api middlewares

--- packages/functions/src/routes.ts
+++ packages/functions/src/routes.ts
@@ -1,12 +1,51 @@
 import { Log } from "@openpromo/core/util/log";
 import { Hono } from "hono";
 import { Ping } from "./ping";
+import { logger } from "hono/logger";
+import { ErrorCodes, VisibleError } from "@openpromo/core/error";
+import { HTTPException } from "hono/http-exception";
 
 const log = Log.create({ namespace: "api" });
 
 export const app = new Hono();
 
-export const routes = app.route("/ping", Ping.route);
+app.use(logger()).use(async (c, next) => {
+  c.header("Cache-Control", "no-store");
+  return next();
+});
+// TODO: handle more middlewares, e.g. auth, cors, etc
+
+export const routes = app.route("/ping", Ping.route).onError((error, c) => {
+  // Handle our custom VisibleError
+  if (error instanceof VisibleError) {
+    // @ts-expect-error
+    return c.json(error.toResponse(), error.statusCode());
+  }
+
+  // Handle HTTP exceptions
+  if (error instanceof HTTPException) {
+    console.error("http error:", error);
+    return c.json(
+      {
+        type: "validation",
+        code: ErrorCodes.Validation.INVALID_PARAMETER,
+        message: "Invalid request",
+      },
+      400,
+    );
+  }
+
+  // Handle any other errors as internal server errors
+  console.error("unhandled error:", error);
+  return c.json(
+    {
+      type: "internal",
+      code: ErrorCodes.Server.INTERNAL_ERROR,
+      message: "Internal server error",
+    },
+    500,
+  );
+});
 
 app.get("/", (c) => {
   log.info("Received request at root endpoint");
