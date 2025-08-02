7617f334d784ccc89280b76c57ddeeca62a212bd
Ruize Li <rl3250@columbia.edu>
2025-08-02 14:59:44 -0700

    streaming api

--- infra/api.ts
+++ infra/api.ts
@@ -13,6 +13,7 @@ export const urls = new sst.Linkable("Urls", {
 const apiFn = new sst.aws.Function("ApiFn", {
   url: true,
   link: [bucket],
+  streaming: !$dev,
   handler: "packages/functions/src/index.handler",
 });
 
