2c27ce62637486e13f839de832b62a77040dc2ca
Ruize Li <rl3250@columbia.edu>
2025-08-03 09:36:50 -0700

    wip rr

--- packages/www_rr/app/root.tsx
+++ packages/www_rr/app/root.tsx
@@ -6,9 +6,9 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import "@openpromo/ui/globals.css";
 
 import type { Route } from "./+types/root";
-import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
