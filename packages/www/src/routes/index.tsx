58f40b958e90604f952e4b2ba05a13b2a8cfc8cd
Ruize Li <rl3250@columbia.edu>
2025-08-02 18:05:42 -0700

    sync

--- packages/www/src/routes/index.tsx
+++ packages/www/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "~/components/ui/button";
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
@@ -7,6 +8,9 @@ function RouteComponent() {
   return (
     <main>
       <h1 className="text-3xl text-blue-500 mb-5">Hello from Openpromo</h1>
+      <Button className="bg-blue-500 text-white hover:bg-blue-600">
+        Click Me
+      </Button>
     </main>
   );
 }
