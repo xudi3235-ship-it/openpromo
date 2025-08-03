41a277ed448069e006d5271566376bc47ba7bf8b
SenkiTK <8024906+senkitk@user.noreply.gitee.com>
2025-08-04 02:45:35 +0800

    add Better Auth things

--- infra/email.ts
+++ infra/email.ts
@@ -1,4 +1,4 @@
-const email = new sst.aws.Email("MyEmail", {
+export const email = new sst.aws.Email("MyEmail", {
     sender: "example.com", // TODO: change this
     dmarc: "v=DMARC1; p=quarantine; adkim=s; aspf=s;",
 });
--- infra/secret.ts
+++ infra/secret.ts
@@ -2,6 +2,8 @@ export const secret = {
     FACEBOOK_APP_ID: new sst.Secret("FACEBOOK_APP_ID"),
     FACEBOOK_APP_SECRET: new sst.Secret("FACEBOOK_APP_SECRET"),
     FACEBOOK_REDIRECT_URI: new sst.Secret("FACEBOOK_REDIRECT_URI"),
+    BETTER_AUTH_SECRET: new sst.Secret("BETTER_AUTH_SECRET"),
+    BETTER_AUTH_URL: new sst.Secret("BETTER_AUTH_URL"),
 };
 
 export const allSecrets = Object.values(secret);
