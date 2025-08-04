677feb7cd55697387c88a95905e35d7348d280f1
SenkiTK <8024906+senkitk@user.noreply.gitee.com>
2025-08-04 20:29:22 +0800

    add pin code provider

--- packages/functions/src/auth/index.ts
+++ packages/functions/src/auth/index.ts
@@ -1,15 +1,36 @@
 import { issuer } from "@openauthjs/openauth";
+import { CodeProvider } from "@openauthjs/openauth/provider/code";
 import { GithubProvider } from "@openauthjs/openauth/provider/github";
+import { CodeUI } from "@openauthjs/openauth/ui/code";
+import { Email } from "@openpromo/core/email/index";
 import { User } from "@openpromo/core/user/index";
 import { subjects } from "./subjects";
 
-// currently we only support github, later on we can add more providers
+// support PIN code and github for now
+
 export const authApp = issuer({
   ttl: {
     access: 60 * 15,           // 15 minutes
     refresh: 60 * 60 * 24 * 7, // 7 days
   },
+
   providers: {
+    code: CodeProvider<{ email: string }>(
+        CodeUI({
+          sendCode: async (user_email, code) => {
+            console.log(`Sending code ${code} to ${user_email.email}`);
+            await Email.send(
+              "no-reply",
+              user_email.email,
+              'OpenPromo Login Code',
+              '', // the email body, but we choose to use styled HTML instead, can enrich it later. see below
+              {
+                html: `<p>Your login verification code is: ${code}</p>`
+              }
+            );
+          },
+        }),
+      ),
     github: GithubProvider({
       clientID: process.env.GITHUB_CLIENT_ID!,
       // a better way to write this would be -- clientID: Resource.GithubClientID.value,
@@ -21,6 +42,14 @@ export const authApp = issuer({
   },
   subjects,
   success: async (ctx, value) => {
+    let email = undefined as string | undefined;
+
+    if (value.provider === "code") {
+        // get email from client side
+        email = value.claims.email;
+        console.log("User logged in with email:", email);
+    }
+
     if (value.provider === "github") {
       const access_token = value.tokenset.access;
       const response = await fetch("https://api.github.com/user/emails", {
@@ -38,9 +67,13 @@ export const authApp = issuer({
       if (!primary.verified) {
         throw new Error("Email not verified by GitHub");
       }
-      const email = primary.email;
 
-      // use this email to check if there's already a user in our DB
+      email = primary.email;
+
+    }
+
+
+      // use email to check if there's already a user in our DB
       if (email) {
         const matching = await User.fromEmail(email);
         if (matching.length === 0) {
@@ -65,7 +98,7 @@ export const authApp = issuer({
           });
         }
       }
-    }
-    throw new Error("Invalid provider");
+
+        throw new Error("Invalid provider");
   }
 })
