d8433d9efe61584f3bb8da3cdeffa76eb61154e7
Ruize Li <rl3250@columbia.edu>
2025-08-05 11:09:29 +0800

    fix secrets in stg

--- scripts/sync_secrets.sh
+++ scripts/sync_secrets.sh
@@ -2,7 +2,12 @@
 set -e
 
 TMPFILE=$(mktemp)
+if [ -z "${SST_STAGE}" ]; then
+    echo "Error: SST_STAGE environment variable is not set."
+    exit 1
+fi
+STAGE="${SST_STAGE}"
 
 doppler secrets download --format=env --no-file > "$TMPFILE"
-pnpm pnpm sst secret load "$TMPFILE"
+pnpm pnpm sst secret load "$TMPFILE" --stage="$STAGE"
 rm "$TMPFILE"
