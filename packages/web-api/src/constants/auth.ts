8a0f11c7d0d1b24daf8c158ea535ebcde3dddcd6
Yuhao-C <yuhaochen0107@gmail.com>
2025-08-22 22:37:08 -0400

    automatically create organization when signing up

---
+++ packages/web-api/src/constants/auth.ts
@@ -0,0 +1,11 @@
+export enum WorkOSOrganizationRole {
+  OWNER = "org_owner",
+  ADMIN = "org_admin",
+  MEMBER = "org_member",
+}
+
+export enum WorkspaceRole {
+  ADMIN = "workspace_admin",
+  EDITOR = "workspace_editor",
+  VIEWER = "workspace_viewer",
+}
--- packages/web-api/src/routes/auth/callback.ts
+++ packages/web-api/src/routes/auth/callback.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { Resource } from "sst";
+import { WorkOSOrganizationRole } from "@/constants/auth";
 import {
   clearAuthStateCookie,
   getAuthState,
@@ -33,11 +34,26 @@ export const callbackRoute = new Hono<ApiEnv>().get("/", async (c) => {
       },
     });
 
-    const { sealedSession } = authenticatedUser;
+    const { sealedSession, user, organizationId } = authenticatedUser;
     if (!sealedSession) {
       throw new Error("No sealed session");
     }
     setSessionCookie(c, sealedSession);
+
+    // Create an organization for the user if it doesn't exist
+    if (!organizationId) {
+      const orgNamePrefix =
+        user.firstName ?? user.lastName ?? user.email.split("@")[0];
+      const orgName = orgNamePrefix ? `${orgNamePrefix}'s Org` : "My Org";
+      const organization = await workOS.organizations.createOrganization({
+        name: orgName,
+      });
+      await workOS.userManagement.createOrganizationMembership({
+        organizationId: organization.id,
+        userId: user.id,
+        roleSlug: WorkOSOrganizationRole.OWNER,
+      });
+    }
 
     const redirectUrl = new URL(returnTo ?? "/", Resource.Urls.site);
     return c.redirect(redirectUrl.toString());
