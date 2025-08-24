5e257a1507be64b091d2b2daa0157db556a7e033
Ruize Li <rl3250@columbia.edu>
2025-08-23 23:14:32 -0700

    impl codegen for private api

---
+++ packages/scripts/src/codegen.ts
@@ -0,0 +1,35 @@
+import { exec } from "node:child_process";
+
+async function main() {
+  const api = process.env.LIQUID_API_URL;
+  if (!api) {
+    console.error("LIQUID_API_URL is not set");
+    return;
+  }
+  const spec = `${api}/docs/openapi.json`;
+  const out = `../web-api/src/generated/api`;
+  const cmd = [
+    "npx",
+    "@hey-api/openapi-ts",
+    "-i",
+    spec,
+    "-o",
+    out,
+    "-c",
+    "@hey-api/client-fetch",
+  ];
+  exec(cmd.join(" "), (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    // biome-ignore lint/suspicious/noConsole: codegen
+    console.debug(`stdout: ${stdout}`);
+  });
+}
+
+main().catch(console.error);
--- packages/web-api/src/routes/api/ping.ts
+++ packages/web-api/src/routes/api/ping.ts
@@ -1,8 +1,12 @@
 import { Hono } from "hono";
+import { ping } from "../../generated/api/sdk.gen";
 import type { ApiEnv } from "../../types";
 
-export const pingRoute = new Hono<ApiEnv>().get("/", (c) => {
+export const pingRoute = new Hono<ApiEnv>().get("/", async (c) => {
   const conn = c.env.HYPERDRIVE.connectionString;
   const message = conn ? "pong" : "no connection";
+  // example usage, calling our api
+  const p = await ping();
+  console.log(p);
   return c.json({ message });
 });
