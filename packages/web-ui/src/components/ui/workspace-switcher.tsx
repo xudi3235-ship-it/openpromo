f6c65575caca89c75878ce874216a92f1847f82d
Yuhao-C <yuhaochen0107@gmail.com>
2025-08-25 22:33:23 -0400

    nit

--- packages/web-ui/src/components/ui/workspace-switcher.tsx
+++ packages/web-ui/src/components/ui/workspace-switcher.tsx
@@ -182,9 +182,7 @@ export function WorkspaceSwitcher({
               <div className="flex size-6 items-center justify-center rounded-md border bg-transparent">
                 <Plus className="size-4" />
               </div>
-              <div className="text-muted-foreground font-medium">
-                Add workspace
-              </div>
+              <div>New workspace</div>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
