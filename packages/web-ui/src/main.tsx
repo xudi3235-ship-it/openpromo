274a798a8b041202659ff8236973c1771ec4b3cf
Ruize Li <rl3250@columbia.edu>
2025-08-26 20:56:30 -0700

    inter font

--- packages/web-ui/src/main.tsx
+++ packages/web-ui/src/main.tsx
@@ -1,3 +1,7 @@
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import "@/components/styles/globals.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
