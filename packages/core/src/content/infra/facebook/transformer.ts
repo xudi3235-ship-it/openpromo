3adb65d33acc46c2237821c50db796edd2d925f1
Ruize Li <rl3250@columbia.edu>
2025-08-30 21:31:52 -0700

    nit

--- packages/core/src/content/infra/facebook/transformer.ts
+++ packages/core/src/content/infra/facebook/transformer.ts
@@ -1,5 +1,5 @@
 import type z from "zod";
-import type { FBFeedPlacementSpec } from "../../schema/placement/facebook";
+import type { FBFeedPlacementSpec } from "../../schema/placement";
 import type { CreateFeedParams } from "./types";
 
 // transforms from/to publishing params and our unified specs
