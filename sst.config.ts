2ad63e7e2942f446e7316bf5301534c1da255307
Ruize Li <rl3250@columbia.edu>
2025-09-02 11:33:36 -0700

    bump cf provider to 6.8.0

--- sst.config.ts
+++ sst.config.ts
@@ -16,7 +16,7 @@ export default $config({
               : undefined,
         },
         cloudflare: {
-          version: "6.6.0",
+          version: "6.8.0",
           apiToken: process.env.CLOUDFLARE_API_TOKEN,
         },
         command: true,
