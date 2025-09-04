dbc61cdcff40b66f4b177e45696052e967aa27e3
Ruize Li <rl3250@columbia.edu>
2025-09-03 18:00:23 -0700

    actor ctx in event def

--- packages/core/src/event.ts
+++ packages/core/src/event.ts
@@ -1,5 +1,11 @@
+import { Actor } from "./actor";
 import { Event } from "./bus/def";
 
 export const defineEvent = Event.builder({
   validator: Event.zodValidator,
+  metadata: () => {
+    return {
+      actor: Actor.use(),
+    };
+  },
 });
