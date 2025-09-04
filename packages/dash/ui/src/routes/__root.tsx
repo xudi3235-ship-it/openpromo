7e4565636c55210ff09dbb3030c8546edbc70058
Yuhao-C <yuhaochen0107@gmail.com>
2025-09-04 01:01:42 -0400

    remove redirect_to from dash now that run worker first is set up correctly

--- packages/dash/ui/src/routes/__root.tsx
+++ packages/dash/ui/src/routes/__root.tsx
@@ -1,12 +1,7 @@
 import { Toaster } from "@openpromo/ui/components/sonner";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {
-  createRootRouteWithContext,
-  Outlet,
-  redirect,
-} from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import z from "zod";
 import { NavigationProgress } from "@/components/navigation-progress";
 import { useTheme } from "@/context/theme-provider";
 import type { AuthContext } from "@/hooks/useAuth";
@@ -16,17 +11,7 @@ interface MyRouterContext {
   auth: AuthContext | undefined;
 }
 
-const schema = z.object({
-  redirect_to: z.string().optional(),
-});
-
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  validateSearch: (search) => schema.parse(search),
-  beforeLoad: async ({ search }) => {
-    if (search.redirect_to) {
-      throw redirect({ to: search.redirect_to });
-    }
-  },
   component: RootLayout,
 });
 
