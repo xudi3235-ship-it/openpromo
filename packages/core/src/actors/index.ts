8dac8a11e5867956712719de0e0cb5d8e6d78d63
Ruize Li <rl3250@columbia.edu>
2025-09-03 22:00:03 -0700

    implement scheduling infra

--- packages/core/src/actors/index.ts
+++ packages/core/src/actors/index.ts
@@ -5,6 +5,7 @@ import {
   type WorkflowStep,
 } from "cloudflare:workers";
 import type { User } from "@workos-inc/node";
+import type { Actor } from "../actor";
 import type { OrganizationRole } from "../workspace/auth";
 
 export type ApiEnv = {
@@ -22,10 +23,22 @@ export type ApiEnv = {
 
 type Bindings = ApiEnv["Bindings"];
 
+type ContentPublishJob = {
+  unifiedContentID: string;
+  pendingContentGroupID: string;
+  metadata: {
+    actor: Actor.WorkspaceUser;
+  };
+  status: "scheduled" | "pending" | "failed";
+  attempts: number;
+  lastAttemptedAt: number | null;
+  nextRetryAt: number | null;
+};
+
 export class Scheduler extends DurableObject<Bindings> {
   constructor(ctx: DurableObjectState, env: Bindings) {
-    // Required, as we're extending the base class.
     super(ctx, env);
+    // ctx.blockConcurrencyWhile(async () => {});
   }
   async sayHello(): Promise<string> {
     const result = this.ctx.storage.sql
@@ -37,9 +50,103 @@ export class Scheduler extends DurableObject<Bindings> {
     this.ctx.storage.setAlarm(Date.now() + 3 * 1000); // 3 seconds from now
     return "scheduled";
   }
+  async scheduleContent(
+    job: Omit<
+      ContentPublishJob,
+      "status" | "attempts" | "lastAttemptedAt" | "nextRetryAt"
+    >,
+  ) {
+    const now = Date.now();
+    const contentJob: ContentPublishJob = {
+      ...job,
+      status: "scheduled",
+      attempts: 0,
+      lastAttemptedAt: null,
+      nextRetryAt: now + 30 * 1000, // first retry in 30 seconds
+    };
+    const { properties: workspaceCtx } = job.metadata.actor;
+    const key = `content:${workspaceCtx.workspaceID}:${job.unifiedContentID}`;
+    await this.ctx.storage.put<ContentPublishJob>(key, contentJob, {
+      allowConcurrency: false,
+    });
+  }
   async alarm() {
-    // will be invoked on alarm, this is core of our scheduling infra.
-    console.log("Alarm fired!");
+    // 1. start clock
+    const now = Date.now();
+    // 2. get the publish jobs from kv
+    const contents = await this.ctx.storage.list<ContentPublishJob>({
+      prefix: "content:",
+    });
+    let nextRetryTime: number | null = null;
+
+    const opts = {
+      allowConcurrency: false, // avoid race condition for each publishing job
+    };
+    for (const [key, content] of contents) {
+      // need to retry for this post
+      if (content.nextRetryAt && content.nextRetryAt > now) {
+        nextRetryTime = Math.min(
+          nextRetryTime ?? Infinity,
+          content.nextRetryAt,
+        );
+        continue;
+      }
+      if (content.attempts >= 3) {
+        // mark as failed
+        await this.ctx.storage.put<ContentPublishJob>(
+          key,
+          {
+            ...content,
+            lastAttemptedAt: now,
+            status: "failed",
+            nextRetryAt: null,
+          },
+          opts,
+        );
+      }
+      // 3. attempt publish, use publishers here
+      try {
+        await this.ctx.storage.put<ContentPublishJob>(
+          key,
+          {
+            ...content,
+            lastAttemptedAt: now,
+            status: "pending",
+            nextRetryAt: Date.now() + 1000,
+          },
+          opts,
+        );
+        const success = this.publish();
+        if (!success) {
+          throw new Error("publish failed");
+        }
+        // success
+        await this.ctx.storage.delete(key, opts);
+      } catch (e) {
+        console.error("publish error", e);
+        content.attempts += 1;
+        content.lastAttemptedAt = now;
+        content.nextRetryAt = now + this.calculateBackoff(content.attempts);
+        content.status = "failed";
+        nextRetryTime = Math.min(
+          nextRetryTime ?? Infinity,
+          content.nextRetryAt ?? Infinity,
+        );
+        await this.ctx.storage.put<ContentPublishJob>(key, content, opts);
+      }
+      // 4. schedule next retry if needed
+      if (nextRetryTime) {
+        this.ctx.storage.setAlarm(nextRetryTime);
+      }
+    }
+  }
+  private publish(): boolean {
+    // TODO: implement publisher
+    return true;
+  }
+
+  private calculateBackoff(attempts: number) {
+    return Math.min(2 ** attempts * 1000, 3600000); // cap at 1 hour
   }
 }
 
