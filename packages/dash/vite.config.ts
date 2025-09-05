c6290fe5f3ea90c2125df9193d2c2cd14ef53ea8
Ruize Li <rl3250@columbia.edu>
2025-09-05 00:15:49 -0700

    use https for localhost dev so that oauth works

--- packages/dash/vite.config.ts
+++ packages/dash/vite.config.ts
@@ -4,13 +4,21 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { analyzer } from "vite-bundle-analyzer";
+import mkcert from "vite-plugin-mkcert";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const flags = {
+  // use local host https, handy for testing OAuth redirects
+  useLocalHttps: true,
+};
 
 export default defineConfig(() => ({
   server: {
     port: 3000,
   },
   plugins: [
+    // @ts-expect-error
+    flags.useLocalHttps && mkcert({ hosts: [] }),
     analyzer({ enabled: false }),
     tsconfigPaths(),
     tanstackRouter({
