f05650bdfbeb87c5e52dc974c8e435e705306f1a
Ruize Li <rl3250@columbia.edu>
2025-09-05 21:46:08 -0700

    use actor sdk storage n alarms

--- packages/core/src/actors/scheduler.ts
+++ packages/core/src/actors/scheduler.ts
@@ -1,9 +1,15 @@
 import { DurableObject } from "cloudflare:workers";
+import { Alarms } from "@cloudflare/actors/alarms";
+import { Storage } from "@cloudflare/actors/storage";
 import type { Bindings } from ".";
 
 export class Scheduler extends DurableObject<Bindings> {
+  storage: Storage;
+  alarms: Alarms<this>;
   constructor(ctx: DurableObjectState, env: Bindings) {
     super(ctx, env);
+    this.storage = new Storage(ctx.storage);
+    this.alarms = new Alarms(ctx, this);
   }
   async sayHello(): Promise<string> {
     const result = this.ctx.storage.sql
@@ -15,8 +21,10 @@ export class Scheduler extends DurableObject<Bindings> {
     this.ctx.storage.setAlarm(Date.now() + 3 * 1000); // 3 seconds from now
     return "scheduled";
   }
-  async alarm() {
-    console.log("Alarm triggered!");
-    // do some periodic tasks here.
+  async alarm(alarmInfo?: AlarmInvocationInfo) {
+    // use actor's multi-alarm
+    if (this.alarms) {
+      await this.alarms.alarm(alarmInfo);
+    }
   }
 }
