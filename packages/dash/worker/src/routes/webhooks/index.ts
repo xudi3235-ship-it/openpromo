46992e2d97144c64dbb2961d4dee9c803c05436b
Yuhao-C <yuhaochen0107@gmail.com>
2025-09-07 06:34:31 -0400

    add onError handler for webhook route

--- packages/dash/worker/src/routes/webhooks/index.ts
+++ packages/dash/worker/src/routes/webhooks/index.ts
@@ -1,8 +1,8 @@
 import type { ApiEnv } from "@openpromo/core/actors/index";
 import { Hono } from "hono";
+import { onError } from "../../helpers/error";
 import { facebookWebhooksRoute } from "./facebook";
 
-export const webhooksRoutes = new Hono<ApiEnv>().route(
-  "/facebook",
-  facebookWebhooksRoute,
-);
+export const webhooksRoutes = new Hono<ApiEnv>()
+  .route("/facebook", facebookWebhooksRoute)
+  .onError(onError);
