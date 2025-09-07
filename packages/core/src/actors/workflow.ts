c889c5f08fd1f1291428fe3aa1c5d4e8c22c99ea
dizys <dev@dizy.cc>
2025-09-06 23:49:49 -0700

    Add core workflow entrypoint wrapper

--- packages/core/src/actors/workflow.ts
+++ packages/core/src/actors/workflow.ts
@@ -2,6 +2,9 @@ import {
   WorkflowEntrypoint,
   type WorkflowEvent,
   type WorkflowStep,
+  type WorkflowStepConfig,
+  type WorkflowStepEvent,
+  type WorkflowTimeoutDuration,
 } from "cloudflare:workers";
 import z from "zod";
 import { EntPendingContent } from "@/domain/content/entity";
@@ -17,40 +20,94 @@ const PublishWorkflowParams = z.object({
 export type PublishWorkflowParams = z.infer<typeof PublishWorkflowParams>;
 const log = Log.create({ namespace: "workflow" });
 
-class WorkflowContext {
-  private constructor(public readonly actor: Actor.WorkspaceUser) {}
-  static provide<T>(
-    actor: Actor.WorkspaceUser,
-    fn: (ctx: WorkflowContext) => Promise<T>,
-  ): Promise<T> {
-    return Actor.provide("workspace_user", { ...actor.properties }, () => {
-      const ctx = new WorkflowContext(actor);
-      return fn(ctx);
+class CoreWorkflowContext {
+  constructor(public readonly actor: Actor.WorkspaceUser) {}
+  provide<T>(fn: (ctx: CoreWorkflowContext) => Promise<T>): Promise<T> {
+    return Actor.provide("workspace_user", { ...this.actor.properties }, () => {
+      return fn(this);
     });
   }
 }
+export class CoreWorkflowStep {
+  constructor(
+    private readonly ctx: CoreWorkflowContext,
+    private readonly step: WorkflowStep,
+  ) {}
 
-export async function stepWithContext<T extends Rpc.Serializable<T>>(
-  step: WorkflowStep,
-  name: string,
-  actor: Actor.WorkspaceUser,
-  fn: (ctx: WorkflowContext) => Promise<T>,
-): Promise<T> {
-  return step.do(name, () => WorkflowContext.provide(actor, fn));
+  async do<T extends Rpc.Serializable<T>>(
+    name: string,
+    fn: () => Promise<T>,
+  ): Promise<T>;
+  async do<T extends Rpc.Serializable<T>>(
+    name: string,
+    config: WorkflowStepConfig,
+    callback: () => Promise<T>,
+  ): Promise<T>;
+  async do<T extends Rpc.Serializable<T>>(
+    name: string,
+    arg2: WorkflowStepConfig | (() => Promise<T>),
+    arg3?: () => Promise<T> | undefined,
+  ): Promise<T> {
+    if (typeof arg3 === "function") {
+      const config = arg2 as WorkflowStepConfig;
+      const fn = arg3 as () => Promise<T>;
+      return this.step.do(name, config, () => this.ctx.provide(fn));
+    } else {
+      const fn = arg2 as () => Promise<T>;
+      return this.step.do(name, () => this.ctx.provide(fn));
+    }
+  }
+
+  async sleep(name: string, duration: WorkflowSleepDuration) {
+    return this.step.sleep(name, duration);
+  }
+
+  async sleepUntil(name: string, timestamp: Date | number) {
+    return this.step.sleepUntil(name, timestamp);
+  }
+
+  async waitForEvent<T extends Rpc.Serializable<T>>(
+    name: string,
+    options: {
+      type: string;
+      timeout?: WorkflowTimeoutDuration | number;
+    },
+  ): Promise<WorkflowStepEvent<T>> {
+    return this.step.waitForEvent(name, options);
+  }
 }
 
-export class PendingContentPublishWorkflow extends WorkflowEntrypoint<
-  Bindings,
-  PublishWorkflowParams
-> {
-  async run(event: WorkflowEvent<PublishWorkflowParams>, step: WorkflowStep) {
-    console.log("// Starting workflow");
-    const { actor, pendingContentID } = event.payload;
+export abstract class CoreWorkflowEntrypoint<
+  WorkflowParams extends Rpc.Serializable<WorkflowParams>,
+> extends WorkflowEntrypoint<Bindings, WorkflowParams> {
+  async run(event: WorkflowEvent<WorkflowParams>, step: WorkflowStep) {
+    const { actor } = event.payload as unknown as {
+      actor: Actor.WorkspaceUser;
+    };
 
-    const { scheduledTime, placement, isDraft } = await stepWithContext(
-      step,
+    const ctx = new CoreWorkflowContext(actor);
+
+    return this.runWithContext(ctx, event, new CoreWorkflowStep(ctx, step));
+  }
+
+  abstract runWithContext(
+    ctx: CoreWorkflowContext,
+    event: WorkflowEvent<WorkflowParams>,
+    step: CoreWorkflowStep,
+  ): Promise<unknown>;
+}
+
+export class PendingContentPublishWorkflow extends CoreWorkflowEntrypoint<PublishWorkflowParams> {
+  async runWithContext(
+    _ctx: CoreWorkflowContext,
+    event: WorkflowEvent<PublishWorkflowParams>,
+    step: CoreWorkflowStep,
+  ) {
+    console.log("// Starting workflow");
+    const { pendingContentID } = event.payload;
+
+    const { scheduledTime, placement, isDraft } = await step.do(
       "fetch content info",
-      actor,
       async () => {
         const c = await EntPendingContent.fromID(pendingContentID);
         return {
