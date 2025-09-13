c67c1301b65381b94df33277ab1737d9af75b65e
dizys <dev@dizy.cc>
2025-09-12 20:04:40 -0700

    ent deserializer as a function

--- packages/core/src/helpers/ent.ts
+++ packages/core/src/helpers/ent.ts
@@ -10,13 +10,16 @@ export abstract class Ent<TData extends Rpc.Serializable<TData>> {
   serialize(): { type: string; data: TData } {
     return { type: (this.constructor as typeof Ent).type, data: this.data };
   }
+}
 
-  deserialize(obj: { type: string; data: TData }): Ent<TData> {
-    if (obj.type !== (this.constructor as typeof Ent).type) {
-      throw new Error(
-        `Type mismatch: expected ${(this.constructor as typeof Ent).type}, got ${obj.type}`,
-      );
-    }
-    return new (this.constructor as Constructor<Ent<TData>>)(obj.data);
+export function deserializeEnt<
+  TEnt extends Ent<TData>,
+  TData extends Rpc.Serializable<TData>,
+>(entClass: Constructor<TEnt>, obj: { type: string; data: TData }): TEnt {
+  if (obj.type !== (entClass as unknown as typeof Ent).type) {
+    throw new Error(
+      `Type mismatch: expected ${(entClass as unknown as typeof Ent).type}, got ${obj.type}`,
+    );
   }
+  return new (entClass as Constructor<TEnt>)(obj.data);
 }
