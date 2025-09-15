7a5e3ea6a66c7b20418b8d04ff985d5d86ee68b0
Ruize Li <rl3250@columbia.edu>
2025-09-15 12:48:19 -0700

    cleanup

--- packages/dash/worker/src/routes/api/workspaces/ping.ts
+++ packages/dash/worker/src/routes/api/workspaces/ping.ts
@@ -6,7 +6,6 @@ import type {
   UnifiedContentInsert,
 } from "@openpromo/core/schemas/content.sql";
 import { Hono } from "hono";
-// import { describeRoute } from "hono-openapi";
 import { withAuth } from "../../../middleware/with-auth";
 import { withWorkspaceRole } from "../../../middleware/with-workspace-role";
 // import { ping } from "../../generated/api/sdk.gen";
@@ -16,45 +15,24 @@ import { withWorkspaceRole } from "../../../middleware/with-workspace-role";
 export const pingRoute = new Hono<ApiEnv>()
   .use(withAuth())
   .use(withWorkspaceRole(WORKSPACE_ROLE.ADMIN))
-  .get(
-    "/",
-    // describeRoute({
-    //   responses: {
-    //     200: {
-    //       description: "pong",
-    //       content: {
-    //         "application/json": {
-    //           schema: {
-    //             type: "object",
-    //             properties: {
-    //               message: { type: "string" },
-    //             },
-    //             required: ["message"],
-    //           },
-    //         },
-    //       },
-    //     },
-    //   },
-    // }),
-    async (c) => {
-      const conn = c.env.HYPERDRIVE.connectionString;
-      const message = conn ? "pong from worker" : "no connection";
+  .get("/", async (c) => {
+    const conn = c.env.HYPERDRIVE.connectionString;
+    const message = conn ? "pong from worker" : "no connection";
 
-      const group: PendingContentGroupInsert = {
+    const group: PendingContentGroupInsert = {
+      workspaceId: "dummy",
+      publishingStatus: "SCHEDULED",
+      pendingContentGroupSpec: {},
+    };
+    const contents: UnifiedContentInsert[] = [
+      {
         workspaceId: "dummy",
         publishingStatus: "SCHEDULED",
-        pendingContentGroupSpec: {},
-      };
-      const contents: UnifiedContentInsert[] = [
-        {
-          workspaceId: "dummy",
-          publishingStatus: "SCHEDULED",
-          placement: "FB_FEED",
-          connectedAccountId: "01K412AGBJJC1W2XXX9HSCH45Z",
-        },
-      ];
-      await EntPendingContentGroup.create({ group, contents });
+        placement: "FB_FEED",
+        connectedAccountId: "01K412AGBJJC1W2XXX9HSCH45Z",
+      },
+    ];
+    await EntPendingContentGroup.create({ group, contents });
 
-      return c.json({ message });
-    },
-  );
+    return c.json({ message });
+  });
