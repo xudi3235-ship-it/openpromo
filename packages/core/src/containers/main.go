55faa3d0ce4b00b35302cf64efc36009dac96556
Ruize Li <rl3250@columbia.edu>
2025-09-18 23:59:33 -0700

    use echo in go

--- packages/core/src/containers/main.go
+++ packages/core/src/containers/main.go
@@ -2,28 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	message := os.Getenv("MESSAGE")
-	instanceId := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
-
-	fmt.Fprintf(w, "Hi, I'm a container and this is my message: %s, and my instance ID is: %s", message, instanceId)
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		message := os.Getenv("MESSAGE")
+		if message == "" {
+			message = "Hello from Echo"
+		}
+		instanceID := os.Getenv("CLOUDFLARE_DEPLOYMENT_ID")
+		return c.String(
+			http.StatusOK,
+			fmt.Sprintf(
+				"Hi, I'm a container and this is my message: %s, and my instance ID is: %s",
+				message,
+				instanceID,
+			),
+		)
+	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Printf("starting server on :%s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("server failed: %v", err)
+	e.Logger.Infof("starting server on :%s", port)
+	if err := e.Start(":" + port); err != nil && err != http.ErrServerClosed {
+		e.Logger.Fatalf("server failed: %v", err)
 	}
 }
