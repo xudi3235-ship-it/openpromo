6622833feb263272ed458cd761a7f975c62e7167
Ruize Li <rl3250@columbia.edu>
2025-09-19 15:42:25 -0700

    edit dialog

--- packages/dash/ui/src/components/composer/media/media-edit-dialog.tsx
+++ packages/dash/ui/src/components/composer/media/media-edit-dialog.tsx
@@ -5,6 +5,17 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@openpromo/ui/components/dialog";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@openpromo/ui/components/sidebar";
 import type { ReactNode } from "react";
 
 type RenderFn = (
@@ -22,13 +33,84 @@ interface MediaEditDialogProps {
   renderAttachment: RenderFn;
 }
 
-export function MediaEditDialog({ editing, onClose }: MediaEditDialogProps) {
+function EditorSidebar() {
+  return (
+    <Sidebar collapsible="none" className="h-full w-64 rounded-bl-lg">
+      <SidebarContent className="flex-1 overflow-y-auto">
+        <SidebarGroup>
+          <SidebarGroupLabel>Basic Edits</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Crop & Resize</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Rotate</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Flip</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Filters & Effects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Brightness</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Contrast</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Saturation</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  <span>Blur</span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
+
+export function MediaEditDialog({
+  editing,
+  onClose,
+  renderAttachment,
+}: MediaEditDialogProps) {
   return (
     <Dialog open={!!editing} onOpenChange={onClose}>
-      <DialogContent className="w-full max-w-6xl md:min-h-[70vh] min-w-[90vw]">
-        <DialogHeader className="space-y-1">
+      <DialogContent className="w-full max-w-4xl h-[80vh] p-0 min-w-[80vw] flex flex-col">
+        <DialogHeader className="p-6 pb-4 flex-shrink-0">
           <DialogTitle>Edit media</DialogTitle>
         </DialogHeader>
+        <div className="flex flex-1 min-h-0">
+          <SidebarProvider className="flex w-64 h-full">
+            <EditorSidebar />
+          </SidebarProvider>
+          <div className="flex-1 p-6 pt-0">
+            {editing &&
+              renderAttachment(editing.attachment, "max-h-full mx-auto", false)}
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   );
