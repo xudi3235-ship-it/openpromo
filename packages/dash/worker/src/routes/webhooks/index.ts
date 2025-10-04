8f4478017870731a0578fc5c4008aab361dc47ac
Yuhao-C <yuhaochen0107@gmail.com>
2025-10-04 17:43:04 -0400

    add ig webhook endpoint to route

--- packages/dash/worker/src/routes/webhooks/index.ts
+++ packages/dash/worker/src/routes/webhooks/index.ts
@@ -2,7 +2,9 @@ import type { ApiEnv } from "@core/helpers/api-env";
 import { Hono } from "hono";
 import { onError } from "../../helpers/error";
 import { facebookWebhooksRoute } from "./facebook";
+import { instagramWebhooksRoute } from "./instagram";
 
 export const webhooksRoutes = new Hono<ApiEnv>()
   .route("/facebook", facebookWebhooksRoute)
+  .route("/instagram", instagramWebhooksRoute)
   .onError(onError);
