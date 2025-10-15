3e5b08344c9942c28d1205f6b8c25045adcb0dd1
Ruize Li <rl3250@columbia.edu>
2025-10-14 19:53:16 -0700

    bump staletime for conn accounts

--- packages/dash/ui/src/queries/connected-account.ts
+++ packages/dash/ui/src/queries/connected-account.ts
@@ -14,6 +14,7 @@ const queryOpts = (workspaceSlug: string) => {
         param: { workspaceSlug },
       }),
     errorMessage: "Failed to load connected accounts",
+    staleTime: 1000 * 60 * 5, // 5 minutes - connected accounts don't change frequently
   });
 };
 
