5921acaaa830eddb8a6622fad9ee03ce383d4722
Ruize Li <rl3250@columbia.edu>
2025-10-29 10:37:14 -0700

    rename webhook routes for easier glob search

--- packages/dash/worker/src/routes/webhooks/index.ts
+++ packages/dash/worker/src/routes/webhooks/index.ts
@@ -1,8 +1,8 @@
 import type { ApiEnv } from "@core/helpers/api-env";
 import { Hono } from "hono";
 import { onError } from "../../helpers/error";
-import { facebookWebhooksRoute } from "./facebook";
-import { instagramWebhooksRoute } from "./instagram";
+import { facebookWebhooksRoute } from "./facebook-webhook-route";
+import { instagramWebhooksRoute } from "./instagram-webhook-route";
 
 export const webhooksRoutes = new Hono<ApiEnv>()
   .route("/facebook", facebookWebhooksRoute)
