0a51e47c4b88d2b80a64e54a9f78e8949428bde9
Ruize Li <rl3250@columbia.edu>
2025-11-08 23:28:01 -0800

    bump cf vite plugin 1.14.0

--- packages/dash/worker/src/index.ts
+++ packages/dash/worker/src/index.ts
@@ -8,7 +8,7 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { bootstrap } from "./middleware/bootstrap";
 import { cfMetadata } from "./middleware/cf-metadata";
-import { logRequestBody } from "./middleware/log-request-body";
+// import { logRequestBody } from "./middleware/log-request-body";
 import { noCache } from "./middleware/no-cache";
 import { apiRoutes } from "./routes/api";
 import { authRoutes } from "./routes/auth";
@@ -17,7 +17,7 @@ import { webhooksRoutes } from "./routes/webhooks";
 
 const app = new Hono()
   .use(logger())
-  .use(logRequestBody())
+  // .use(logRequestBody())
   .use(cfMetadata())
   .use(noCache())
   .use(bootstrap())
